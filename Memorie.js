e9f293754819124eb8d33781deb19b1092866beb
FST601 <finn.stawitzki@itc-studenten.de>
2022-12-06 16:28:18 +0100

    Neu Angefangen

--- Memorie.js
+++ Memorie.js
@@ -1,18 +1,6 @@
-let punkteSpieler1 = 0;
-let punkteSpieler2 = 0;
-
 function getPlayer () {
     let spieler1 = prompt("Spieler 1 bitte Namen eingeben");
     let spieler2 = prompt("Spieler 2 bitte Namen eingeben");
     document.getElementById('spielerEins').innerHTML += spieler1;
     document.getElementById('spielerZwei').innerHTML += spieler2;
-}
-
-function test() {
-    console.log("test")
-}
-
-function test2() {
-    document.getElementById('2').src = 'Count-Dooku_4f552149.jpeg';
-    console.log("funktioniert")
 }
